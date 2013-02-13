ec3a93647baab396a11d24986c593d11216a068a
Neil Hancock <neil@neilhancock.co.uk>
2013-02-13 22:15:39 +0000

    Moved the java files into src, and they now should do the job

---
+++ portfolio/java/src/WordSort.java
@@ -0,0 +1,38 @@
+package uk.co.neilhancock;
+ 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+ 
+public class WordSort {
+    public static void main(String[] args) {
+    	String prevword = "";
+	String commasep = "";        
+        // Check args
+    	if (args.length == 0) {
+    		System.out.println("No arguments!");
+    		return;
+    	}
+
+    	// Pull args into a list    	
+        List<String> list = new ArrayList<String>();       
+        for (String arg : args) {
+        	// System.out.println(arg);
+        	list.add(arg);   
+        }
+
+        // Sort list
+        Collections.sort(list, new WordSortComparator());
+
+        // Output results, skipping over dupes as we go
+        for (String word : list) {
+
+        	if (!word.toLowerCase().equals(prevword)) {
+        		// Finish last word with a comma       			
+        		System.out.print(commasep+word);
+        		prevword = word.toLowerCase();
+        		commasep = ", ";
+        	}
+        }
+    }
+}
